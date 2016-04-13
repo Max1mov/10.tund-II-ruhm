3a590d8c80ec97604c64c9d4f8a8e0f582d426c3
Vitali Maksimov <vitamak@greeny.cs.tlu.ee>
2016-04-13 10:42:04 +0300

    ...

---
+++ feed.js
@@ -0,0 +1,51 @@
+$(function() {
+  
+  //laetud
+    
+  getTweets();
+  
+  
+}); 
+
+
+function getTweets(){
+	
+	//ajax
+
+	$.ajax({
+		url: "getfeed.php",
+		success: function(data){
+			
+			//stringi teen massiiviks
+			var array = JSON.parse(data).statuses;
+			
+			console.log(array);
+			printTweets(array);
+			
+		},
+		error: function(error){
+			console.log(error);
+		}
+	});
+	
+}
+
+function printTweets(newTweets){
+	
+	var html = '';
+	
+	$(newTweets).each(function(i, tweet){
+		
+		html += '<div class="item">'+
+			
+			'<div class="profile-image" style="background-image:url('+tweet.user.profile_image_url+')"></div>'+
+			'<p>'+tweet.user.name+'</p>'+
+			'<p>'+tweet.text+'</p>'+
+
+		'</div>';
+		
+	});
+	
+	$("#content").append( $(html) );
+	
+}
